9d113f9da614a89eda99a49b96730ad32b35a0f4
Rahul Krishnan <emailrahulk64@gmail.com>
2017-12-02 13:09:49 -0700

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(app.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
