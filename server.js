44d80d3296f25e824b68c41791fa4d16a1caee31
Rahul Krishnan <emailrahulk64@gmail.com>
2017-12-02 13:07:44 -0700

    Add Tests for Socket & Gyro

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,8 +14,12 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('chat message', function(data){
+    console.log(data);
+  });
 });
 
-http.listen(3000, '0.0.0.0' function(){
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
